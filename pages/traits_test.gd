0cd53e0ddacd3c67feffdad52a82c3ea531aa7c0
GIRA-AKI <roger.2020.y@gmail.com>
2024-12-10 20:18:46 +0700

    improve  trait selection

--- pages/traits_test.gd
+++ pages/traits_test.gd
@@ -6,17 +6,25 @@ var list_item = preload("res://pages/traits_set.tscn")
 @onready var traits_good = $Control/traits_good/MarginContainer/ScrollContainer/VBoxContainer
 @onready var traits_bad = $Control/traits_bad/MarginContainer/ScrollContainer/VBoxContainer
 @onready var traits_use = $Control/use_traits/MarginContainer/ScrollContainer/VBoxContainer
-
+@onready var available_points = $Control/available_points/Label2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	loadAllTable()
 	pass
+
+func calAvailablePoints():
+	var current_point = 10
+	for i in BaseSystem.playerTraits:
+		current_point += i["point"]
+	available_points.text = str(current_point)
+	print(current_point)
+	print("available_points was trigger")
 	
 func loadAllTable()-> void:
-	
 	loadTraits(traits_good,BaseSystem.good_traits)
 	loadTraits(traits_bad,BaseSystem.bad_traits)
 	loadTraits(traits_use,BaseSystem.playerTraits,true)
+	calAvailablePoints();
 
 func loadTraits(table, traits:Array, isPlayerTraits:bool = false) -> void:
 	
